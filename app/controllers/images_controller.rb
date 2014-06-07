12c32cd8b79662b5ea3af9ba0169cef23814812c
Prelang Builder <builder@prelang.com>
2014-06-07 23:34:09 +0000

    Added #vote action to set model before_action in the votable model's controller

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,5 +1,5 @@
 class ImagesController < ApplicationController
-  before_action :set_image, only: [:show, :edit, :update, :destroy]
+  before_action :set_image, only: [:show, :edit, :update, :destroy, :vote]
 
   # GET /images
   # GET /images.json
