0ebde529ca532295e2e736d507c877e019b7d533
Prelang Builder <builder@prelang.com>
2014-06-07 23:29:51 +0000

    Initial commit; clean Rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RissaPhotography::Application.config.secret_key_base = '17b1fb3b7046ec654b69a3be301604a3b082e5a3994fa0f1da8fe44280bb1bd245fbcd24dbb5a74370b044da16484e64dc591dfced893ef5b436a2e473ddf8db'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RissaPhotography::Application.config.session_store :cookie_store, key: '_rissa_photography_session'
